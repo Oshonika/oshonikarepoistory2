27a6a4eef5987c10ef2ea8e862b20bfa97cdd540
Oshonika <Oshonika@LAPTOP-59QOCOB2>
2020-01-04 22:34:13 +0530

    I am pushing the whole project

---
+++ Project_1/src/pkg1/Student.java
@@ -0,0 +1,23 @@
+package pkg1;
+
+public class Student {
+	int age;
+	int roll_no;
+	
+	public void display() 
+	{
+	System.out.println("Welcome to all of you");	
+
+    }
+	public static void main(String[] args) 
+	{
+		Student deepak=new Student();
+		deepak.age=15;
+		deepak.roll_no=12;
+		System.out.println(deepak.age);
+		System.out.println(deepak.roll_no);
+		deepak.display();	
+	    
+	}
+}
+
